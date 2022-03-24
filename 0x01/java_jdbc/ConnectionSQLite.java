05ad62e01cd96d80dc06310245e9a1c7d1b22a41
Paula Martins <paulastsmartins@gmail.com>
2022-03-24 11:09:05 -0300

    criar

---
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -0,0 +1,30 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionSQLite {
+
+	public static void main(String[] args) throws SQLException {
+		initConnection();
+	}
+	
+	private static void initConnection() throws SQLException {
+		Connection conn = null;
+		try {
+		String url = "jdbc:sqlite:sqlite_database_2022.db";
+		conn = DriverManager.getConnection(url);
+		System.out.println("Connection to SQLite has been established.");
+	} catch (SQLException e) {
+		System.out.println(e.getMessage());
+	} finally {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+}
+}
