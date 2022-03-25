da5e670ce193d231c65bb717605ab2e1e8c5ce7e
Paula Martins <paulastsmartins@gmail.com>
2022-03-25 08:23:59 -0300

    relacionamento

---
+++ 0x02/java_jdbc_manipulation/Cliente.java
@@ -0,0 +1,31 @@
+public class Cliente {
+	private String nome;
+	private Integer idade;
+	private String CPF;
+	private String RG;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getRG() {
+		return RG;
+	}
+	public void setRG(String rG) {
+		RG = rG;
+	}
+		
+}
